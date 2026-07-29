e27528053acc6696c06ba4a20545eb245f657884
elysee <mboussaemmanuelito@gmail.com>
2026-07-30 00:57:20 +0100

    feat(ui): refactor Button and ButtonAdd classes for improved label handling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,13 @@
 export class Button {
+  /** @type {string} */
+  label;
+
+  /**
+   * @param {string} label
+   */
+  constructor(label) {
+    this.label = label;
+  }
   onMount() {
     console.log("mount");
   }
--- src/components/ButtonAdd.js
+++ src/components/ButtonAdd.js
@@ -1,18 +1,6 @@
 import { Button } from "./Button.js";
 
 export class ButtonAdd extends Button {
-  /** @type {string} */
-  label;
-
-  /**
-   *
-   * @param {string} label
-   */
-  constructor(label) {
-    super(label);
-    this.label = label;
-  }
-
   build() {
     const button = document.createElement("button");
     button.classList.add("counter");
